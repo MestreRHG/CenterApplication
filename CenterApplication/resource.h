4f3e7d4a2b4f17fe941c5927615e4f76dfa27620
MestreRHG <38073948+MestreRHG@users.noreply.github.com>
2023-10-08 20:03:24 +0100

    Added new icon

--- CenterApplication/resource.h
+++ CenterApplication/resource.h
@@ -2,7 +2,8 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDI_ICON                        101
+#define IDI_ICON1                       103
+#define IDI_ICON                        103
 #define ID__CENTERCURRENTAPPLICATION    40001
 #define ID__OPTIONS                     40002
 #define ID__EXIT                        40003
@@ -11,7 +12,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
